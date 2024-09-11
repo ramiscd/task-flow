3c5f28071628398063013455010673c431d75df4
Ramis Damasceno <ramis1998d@gmail.com>
2024-09-11 16:21:31 -0300

    feat: add tailwind to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :cards
   resources :lists
   resources :boards
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get "/home", to: "home#index"
 
   # Defines the root path route ("/")
   # root "posts#index"

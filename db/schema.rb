867c27ffb72413f27d18f022ce0ce9b8a251087d
Ramis Damasceno <ramis1998d@gmail.com>
2024-09-11 14:33:27 -0300

    feat: create scaffold board

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_11_172045) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_11_173130) do
+  create_table "boards", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date_created"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_11_172045) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "boards", "users"
 end

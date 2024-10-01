5790176a32f7ae6fe08be25b1e0790bc56321d88
Ramis Damasceno <ramis1998d@gmail.com>
2024-10-01 09:10:54 -0300

    need to fix this

--- app/services/card_rpc_server.rb
+++ app/services/card_rpc_server.rb
@@ -1,27 +1,26 @@
 require 'bunny'
 
 class CardRPCServer
+  begin
+      connection = Bunny.new
+      connection.start
+      puts "Conexão com RabbitMQ estabelecida com sucesso!"
 
-  def initialize
-    connection = Bunny.new
-    connection.start
+      channel = connection.create_channel
+      queue = channel.queue('rpc_card_queue')
 
-    channel = connection.create_channel
-    queue = channel.queue('rpc_card_queue')
+      puts '[*] Waiting for RPC requests...'
 
-    puts '[*] Waiting for RPC requests...'
+      queue.subscribe(block: true) do |delivery_info, properties, payload|
+        card_data = JSON.parse(payload)
+        response = move_card(card_data['card_id'], card_data['new_list_id'])
 
-    queue.subscribe(block: true) do |delivery_info, properties, payload|
-      card_data = JSON.parse(payload)
-      response = move_card(card_data['card_id'], card_data['new_list_id'])
-
-      channel.default_exchange.publish(
-        response.to_json,
-        routing_key: properties.reply_to,
-        correlation_id: properties.correlation_id
-      )
+        channel.default_exchange.publish(
+          response.to_json,
+          routing_key: properties.reply_to,
+          correlation_id: properties.correlation_id
+        )
     end
-  end
 
   def move_card(card_id, new_list_id)
     card = Card.find(card_id)
@@ -30,4 +29,18 @@ class CardRPCServer
   rescue => e
     { status: 'error', message: e.message}
   end
+rescue => e
+  puts "Erro ao conectar: #{e.message}"
 end
+end
+
+
+
+# begin
+#   connection = Bunny.new
+#   connection.start
+#   puts "Conexão com RabbitMQ estabelecida com sucesso!"
+#   connection.close
+# rescue => e
+#   puts "Erro ao conectar: #{e.message}"
+# end
